e0e5e4e0ee936f0878afc926faa04e09ba2ad4c5
Entav78 <hkljos_238@hotmail.com>
2025-08-15 11:05:13 +0200

    ny tryggivannLinks

--- src/data/tryggivannLinks.ts
+++ src/data/tryggivannLinks.ts
@@ -1,8 +1,8 @@
 export const tryggivannLinks = {
-  hval: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=2',
-  skilpadde: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=3',
-  pingvin: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=4',
-  selungen: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=5',
-  sel: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=6',
-  sjolove: 'https://tryggivann.no/finnkursklubb.aspx?klubbid=425&kurstype=7',
+  hval: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=2',
+  skilpadde: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=3',
+  pingvin: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=4',
+  selungen: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=5',
+  sel: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=6',
+  sjolove: 'https://tryggivann.no/finnkursklubb.aspx?arr=68&kurstype=7',
 };
